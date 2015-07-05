c4209e9a42bac7704d53babb338f3dea0d429758
Shaowei <zsw22@vip.qq.com>
2015-07-06 00:13:16 +1000

    start

---
+++ Download Web Content/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Download Web Content
+//
+//  Created by Shaowei Zhang on 15/7/6.
+//  Copyright © 2015年 Shaowei Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
